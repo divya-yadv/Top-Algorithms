a8a5484d8a0e545b0c030d2f3a686210f7dd4430
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-04 15:20:47 +0530

    Update Kadane's.cpp

--- More/Kadane's.cpp
+++ More/Kadane's.cpp
@@ -1,1 +1,14 @@
 
+int maxSubArray(vector<int>& nums) {
+        int max_sum = nums[0];
+        int current_sum = nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+          // incase of negative elements to see if we continue in the subarray or start a new subarray
+            current_sum = max(current_sum+nums[i],nums[i]);
+          // take the max
+            max_sum = max(max_sum,current_sum);
+        }
+        return max_sum;
+    }
+};
